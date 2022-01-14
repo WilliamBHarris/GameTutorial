93122e8f23a791922a34c74ab5cca053236e55a1
Will Harris <bharris010486@gmail.com>
2022-01-13 20:53:51 -0500

    commit

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//
+// 20MIN
 //            Create an interface called:
 //?                   CharacterInterface
 //
@@ -24,3 +24,152 @@
 //            Create a new instance of the Player class and the Enemy class passing along the correct arguments for (name..health..attack..etc).
 //
 //            Call the attack function on the new Player instance, and pass in the new Enemy instance, see if it functions correctly.
+
+interface CharacterInterface {
+  name: string;
+  health: number;
+  maxHealth: number;
+  attack: number;
+  defense: number;
+  accuracy: number;
+  healthBar: HealthIcon[];
+  attackTarget(target: CharacterInterface): void;
+  showHealth() : void;
+}
+
+type HealthIcon = "🟩" | "🟥";
+
+class Character implements CharacterInterface {
+  healthBar: HealthIcon[] = [
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+    "🟩",
+  ];
+
+  maxHealth: number = this.health;
+  constructor(
+    public name: string,
+    public health: number,
+    public attack: number,
+    public defense: number,
+    public accuracy: number
+  ) {}
+
+  attackTarget(target: CharacterInterface) {
+    //  Check attacker health
+    if (this.health < 0) {
+      return;
+    } else {
+      //  Check target health
+      if (target.health > 0) {
+        //   Attack Landed
+        if (Math.random() > 1 - this.accuracy) {
+          let damage: number =
+            this.attack * (100 / 100 + target.defense) +
+            Math.floor(Math.random() * 5);
+          console.log(
+            `${this.name} attacked ${target.name} with ${damage} damage 💥 \n`
+          );
+          target.health -= damage;
+          // Checking target's health percent
+          let targetHealthPercent = (target.health / target.maxHealth).toFixed(
+            2
+          );
+          if (targetHealthPercent.toString()[0] == "-") {
+            for (let x = 0; x < target.healthBar.length; x++) {
+              target.healthBar[x] = "🟥";
+            }
+          } else {
+            target.healthBar = [
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+              "🟥",
+            ];
+            for (let y = 0; y < +targetHealthPercent.toString()[2]; y++) {
+              target.healthBar[y] = "🟩";
+            }
+            target.showHealth();
+          }
+
+          if (target.health < 0) {
+            //  Check if target is still alive
+            console.log(
+              `${target.name} was defeated by ${this.name}...💀💀💀💀\n`
+            );
+          }
+        } else {
+          console.log(`${this.name}'s Attack missed! ❌ \n`);
+        }
+      }
+    }
+}
+
+async rest(){
+    console.log("Resting")
+
+    return new Promise((resolve, reject): void => {
+            setTimeout(() => {
+                    console.log("rest is complete.  -HP RESTORED--");
+                    this.health = this.maxHealth;
+            }, 3000)
+    })
+}
+
+  showHealth() {
+    console.log(`[${this.name}]`);
+    console.log(`[${this.healthBar.join("")}]`);
+    console.log(`HP: ${this.health}/${this.maxHealth} \n`);
+  }
+}
+class Player extends Character {
+  constructor(
+    public name: string,
+    public health: number,
+    public attack: number,
+    public defense: number,
+    public accuracy: number
+  ) {
+    super(name, health, attack, defense, accuracy);
+  }
+}
+class Enemy extends Character {
+  constructor(
+    public name: string,
+    public health: number,
+    public attack: number,
+    public defense: number,
+    public accuracy: number
+  ) {
+    super(name, health, attack, defense, accuracy);
+  }
+}
+
+const Player1: Player = new Player("🗡 Dragon_Slayer", 3130, 50, 30, 0.5);
+const AI_Entry_Creature: Enemy = new Enemy("🔥🐰 Fire Rabbit", 500, 5, 7, 0.3);
+const AI_Ice_Dragon_Boss: Enemy = new Enemy(
+  "❄🐲 Ice Dragon",
+  5050,
+  25,
+  18,
+  0.4
+);
+
+for (let i = 1; i < 10; i++) {
+  Player1.attackTarget(AI_Entry_Creature);
+}
+//break - player1 refills health
+// now goes against boss
